52621745ba2d532a1112e97120a29dbe9b8668e5
Pandu <pandu@berkeley.edu>
2013-08-09 14:04:22 -0700

    Move .vimrc, .gvimrc to vimrc, gvimrc

--- dotvim-install.sh
+++ dotvim-install.sh
@@ -2,8 +2,8 @@
 set -e
 cd ~
 git clone https://github.com/prendradjaja/vim-config.git ~/.vim
-ln -s ~/.vim/.vimrc ~/.vimrc
-ln -s ~/.vim/.gvimrc ~/.gvimrc
+ln -s ~/.vim/vimrc ~/.vimrc
+ln -s ~/.vim/gvimrc ~/.gvimrc
 cd ~/.vim
 git submodule init
 git submodule update
